3fcc5177a81dd331ab34d57461a280262834cf5d
OTHMANI <najmothmani@gmail.com>
2020-11-19 11:57:28 +0100

    Making use of UserRest and UserDto in RestController

---
+++ src/main/java/com/starapp/ws/mobileappws/service/UserService.java
@@ -0,0 +1,7 @@
+package com.starapp.ws.mobileappws.service;
+
+import com.starapp.ws.mobileappws.shared.dto.UserDto;
+
+public interface UserService {
+    UserDto createUser(UserDto userDto);
+}
--- src/main/java/com/starapp/ws/mobileappws/ui/controller/UserController.java
+++ src/main/java/com/starapp/ws/mobileappws/ui/controller/UserController.java
@@ -1,31 +1,48 @@
 package com.starapp.ws.mobileappws.ui.controller;
 
 
+import com.starapp.ws.mobileappws.service.UserService;
+import com.starapp.ws.mobileappws.shared.dto.UserDto;
+import com.starapp.ws.mobileappws.ui.model.request.UserDetailsRequestModel;
+import com.starapp.ws.mobileappws.ui.model.response.UserRest;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 
 @RestController
 @RequestMapping("users") // http//:localhost:8080/users
 public class UserController {
-    
+
+    @Autowired
+    UserService userService;
+
     @GetMapping
-    public String getUser(){
+    public String getUser() {
         return "get user was called";
     }
-    
+
     @PostMapping
-    public String createUser () {
-        return "create user was called";
+    public UserRest createUser(@RequestBody UserDetailsRequestModel userDetails) {
+        UserRest returnValue = new UserRest();
+
+        UserDto userDto = new UserDto();
+        BeanUtils.copyProperties(userDetails, userDto);
+
+        UserDto createdUser = userService.createUser(userDto);
+        BeanUtils.copyProperties(createdUser, returnValue);
+
+        return returnValue;
     }
 
     @PutMapping
-    public String updateUser () {
+    public String updateUser() {
         return "update user was called";
     }
 
     @DeleteMapping
-    public String deleteUser () {
+    public String deleteUser() {
         return "delete user was called";
     }
-    
+
 }
