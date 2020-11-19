6caa9562c58e90a21c4175a9f84ffe0c69032ab8
OTHMANI <najmothmani@gmail.com>
2020-11-19 12:03:59 +0100

    Implementing UserRepository class

---
+++ src/main/java/com/starapp/ws/mobileappws/UserRepository.java
@@ -0,0 +1,10 @@
+package com.starapp.ws.mobileappws;
+
+import com.starapp.ws.mobileappws.io.entity.UserEntity;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<UserEntity, Long> {
+    UserEntity findUserByEmail(String email);
+}
