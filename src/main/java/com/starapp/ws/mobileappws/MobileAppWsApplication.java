89fa05584386621b12abb67ee12a9f4ac0784fec
OTHMANI <najmothmani@gmail.com>
2020-12-24 11:49:22 +0100

    Reading Token Secret from file : application.properties

--- src/main/java/com/starapp/ws/mobileappws/MobileAppWsApplication.java
+++ src/main/java/com/starapp/ws/mobileappws/MobileAppWsApplication.java
@@ -1,5 +1,6 @@
 package com.starapp.ws.mobileappws;
 
+import com.starapp.ws.mobileappws.security.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -22,4 +23,8 @@ public class MobileAppWsApplication {
         return new SpringApplicationContext();
     }
 
+    @Bean(name = "AppProperties")
+    public AppProperties appProperties() {
+        return new AppProperties();
+    }
 }
