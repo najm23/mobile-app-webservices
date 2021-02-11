e3012339ea1c6a417e3b9b9438a731e3ab0d41fb
najm23 <najmothmani@gmail.com>
2021-02-11 09:44:29 +0100

    Return Custom Error Object

--- src/main/java/com/starapp/ws/mobileappws/exceptions/AppExceptionsHandler.java
+++ src/main/java/com/starapp/ws/mobileappws/exceptions/AppExceptionsHandler.java
@@ -1,5 +1,6 @@
 package com.starapp.ws.mobileappws.exceptions;
 
+import com.starapp.ws.mobileappws.ui.model.response.ErrorMessage;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,11 +8,16 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
+import java.util.Date;
+
 @ControllerAdvice
 public class AppExceptionsHandler {
 
     @ExceptionHandler(value = {UserServiceException.class})
     public ResponseEntity<Object> handleUserServiceException(UserServiceException exception, WebRequest request) {
-        return new ResponseEntity<>(exception.getMessage(), new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+
+        ErrorMessage errorMessage = new ErrorMessage(new Date(), exception.getMessage());
+
+        return new ResponseEntity<>(errorMessage, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
